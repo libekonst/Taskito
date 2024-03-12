ab427b065e452b1f6f48a7b003cfc1061f082948
Konstantinos Liberopoulos <libekonst@gmail.com>
2024-03-12 22:55:59 +0200

    Initial Commit

---
+++ Taskito/TaskitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskitoApp.swift
+//  Taskito
+//
+//  Created by Konstantinos Liberopoulos on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskitoUITests/TaskitoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TaskitoUITestsLaunchTests.swift
+//  TaskitoUITests
+//
+//  Created by Konstantinos Liberopoulos on 12/3/24.
+//
+
+import XCTest
+
+final class TaskitoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
