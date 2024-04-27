941b4ddd2e77379a56fe3570887ad8148a13addb
Konstantinos Liberopoulos <libekonst@gmail.com>
2024-04-27 16:35:57 +0300

    feat: form focus interaction

--- Taskito/Views/FormView.swift
+++ Taskito/Views/FormView.swift
@@ -5,6 +5,7 @@
 //  Created by Konstantinos Liberopoulos on 22/3/24.
 //
 
+import Combine
 import SwiftUI
 
 struct FormView: View {
@@ -12,6 +13,8 @@ struct FormView: View {
 
     @State private var minutes = 25
     @State private var seconds = 00
+
+    @FocusState private var focus: FocusedField?
 
     var body: some View {
         VStack {
@@ -23,6 +26,18 @@ struct FormView: View {
                         formatter: TimeFormatter.Restrictions.minutes
                     )
                     .textFieldStyle(PlainNumericInputStyle(justify: .trailing))
+                    .focused($focus, equals: .minutes)
+                    .onAppear {
+                        self.focus = .minutes
+                    }
+                    .onReceive(Just(minutes), perform: { x in
+                        let count = String(minutes).count
+                        let upperLimit = 2
+
+                        if count == upperLimit {
+                            focus = .seconds
+                        }
+                    })
 
                     Text(":")
                         .font(.system(size: 72, weight: .thin, design: .rounded))
@@ -32,10 +47,10 @@ struct FormView: View {
                     TextField(
                         "Seconds",
                         value: $seconds,
-                        formatter: TimeFormatter.Restrictions.seconds
+                        formatter: TimeFormatter.Restrictions.minutes
                     )
                     .textFieldStyle(PlainNumericInputStyle())
-
+                    .focused($focus, equals: .seconds)
                 })
 
                 HStack {
@@ -53,13 +68,16 @@ struct FormView: View {
 
                     Spacer()
                 }
-
-            }.onSubmit {
-                print("form submitted")
+            }
+            .onSubmit {
                 onSubmit(minutes, seconds)
             }
         }.frame(maxWidth: .infinity, maxHeight: .infinity)
     }
+}
+
+private enum FocusedField: Hashable {
+    case minutes, seconds
 }
 
 private struct PlainNumericInputStyle: TextFieldStyle {
